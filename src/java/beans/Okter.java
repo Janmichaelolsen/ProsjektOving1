8a79f725b186773cedd01bb70ae798a98b96d492
Jan Michael <Olsen>
2012-10-25 12:46:42 +0200

    Login og Arraylist

--- src/java/beans/Okter.java
+++ src/java/beans/Okter.java
@@ -2,17 +2,24 @@ package beans;
 
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import java.util.Date;
-import java.util.Locale;
-import javax.faces.component.UIViewRoot;
-import javax.faces.context.FacesContext;
 
 @Named("list")
 @SessionScoped
 
 public class Okter implements Serializable {
-    
 
+    ArrayList<TreningsOkt> liste = new ArrayList<TreningsOkt>();
+    
+    public Okter(){
+    }
+
+    public void regNyOkt(int oktnr, String dato, int varighet, String kategori, String tekst) {
+        TreningsOkt nyokt = new TreningsOkt(oktnr, dato, varighet, kategori, tekst);
+        liste.add(nyokt);
+    }
+    public ArrayList getListe() { return liste; }
 }
