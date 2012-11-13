ac64c6ea1b6596042562f5c35595d50e8c83ae6d
Jan Michael <Olsen>
2012-11-13 15:35:19 +0100

    innlogging

---
+++ src/java/Script/Dbscript.sql
@@ -0,0 +1,55 @@
+DROP TABLE trening;
+DROP TABLE kategori;
+DROP TABLE bruker;
+DROP TABLE tilleggskat;
+ 
+CREATE TABLE trening(
+  oktnr        INTEGER NOT NULL,
+  dato         DATE NOT NULL,
+  varighet     INTEGER,
+  kategorinavn VARCHAR(15),
+  tekst        VARCHAR(30),
+  brukernavn   VARCHAR(10),
+  CONSTRAINT trenings_pk PRIMARY KEY(oktnr));
+ 
+CREATE TABLE bruker(
+  brukernavn VARCHAR(10) PRIMARY KEY,
+  passord    VARCHAR(10) NOT NULL);
+ 
+CREATE TABLE kategori(
+  kategorinavn VARCHAR(15)PRIMARY KEY);
+
+CREATE TABLE tilleggskat(
+  tilleggkat VARCHAR(15)PRIMARY KEY);
+
+CREATE TABLE rolle(
+   brukernavn VARCHAR(10),
+   rolle      VARCHAR(10),
+   CONSTRAINT rolle_pk PRIMARY KEY(brukernavn, rolle));
+  
+ALTER TABLE trening
+  ADD CONSTRAINT trening_fk1 FOREIGN KEY (kategorinavn)
+  REFERENCES kategori;
+  
+ALTER TABLE trening
+  ADD CONSTRAINT trening_fk2 FOREIGN KEY (brukernavn)
+  REFERENCES bruker;
+
+ALTER TABLE rolle
+  ADD CONSTRAINT rolle_fk1 FOREIGN KEY (brukernavn)
+  REFERENCES bruker;
+
+INSERT INTO rolle(brukernavn, rolle) VALUES('anne', 'bruker');
+INSERT INTO rolle(brukernavn, rolle) VALUES('tore', 'bruker');
+INSERT INTO kategori(kategorinavn) VALUES('Sykling');
+INSERT INTO kategori(kategorinavn) VALUES('Styrke');
+INSERT INTO kategori(kategorinavn) VALUES('Svømming');
+INSERT INTO kategori(kategorinavn) VALUES('Løping');
+INSERT INTO kategori(kategorinavn) VALUES('Yoga');
+INSERT INTO kategori(kategorinavn) VALUES('Biking');
+INSERT INTO kategori(kategorinavn) VALUES('Strength');
+INSERT INTO kategori(kategorinavn) VALUES('Swimming');
+INSERT INTO kategori(kategorinavn) VALUES('Running');
+ 
+INSERT INTO bruker(brukernavn, passord) VALUES('anne', 'xyz_1b');
+INSERT INTO bruker(brukernavn, passord) VALUES('tore', 'xcg_5');
